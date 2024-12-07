3e1a555d6a11f76c5d4b70e2cc31066edceb747b
famfrank1298 <68818256+famfrank1298@users.noreply.github.com>
2024-12-07 17:09:44 -0600

    added web analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main>{children}</main>
-        <SpeedInsights />
         <Toaster />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
